09e41a1f47e9d3a554f4a29af0563bd0ed5a6590
Tamil arsan <yourGitHub@email.com>
2023-05-22 16:16:00 +0530

    medicalapp

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import pickle
+from sklearn.preprocessing import StandardScaler
+st.title("Medical Diagnostic App")
+st.markdown("Does the women have diabetes or not? 👩‍⚕️")
+
+# step 1 load the pickled model
+
+model = open("rfc.pickle","rb")
+clf = pickle.load(model)
+model.close()
+
+
+# step2 get the user input from the front end
+pregs= st.number_input('Pregnancies',0,20,step = 1) 
+glucose = st.slider('Glucose',40,200,40) 
+bp = st.slider('BloodPressure',20,140,20)
+skin = st.slider('SkinThickness',5,100,5)
+insulin = st.slider('Insulin', 14.0,846.0,14.0)
+bmi = st.slider('BMI',15.0,70.0,15.0)
+dpf = st.slider('DiabetesPedigreeFunction', 0.05,2.50,0.05)
+age = st.slider('Age',21,90,21)
+
+# step3 : converting user input to model input 
+
+data = {'Pregnancies': pregs,
+        'Glucose' : glucose, 
+        'BloodPressure' : bp, 
+        'SkinThickness': skin,
+        'Insulin' : insulin,
+       'BMI': bmi,
+        'DiabetesPedigreeFunction': dpf,
+        'Age':age }
+input_data = pd.DataFrame([data])
+# st.write(input_data)
+
+# step 4: get the modle prediction and print it 
+
+prediction = clf.predict(input_data)[0]
+if st.button("Prediction"):
+    if prediction ==1:
+        st.subheader("The woman has diabetes")
+    if prediction==0:
+        st.subheader("The woman is Healthy")
